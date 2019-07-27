31513a5c05efdaf7e2075dc280faa738694db17c
Robert Leahy <rleahy@rleahy.ca>
2019-07-27 15:55:10 +0000

    basic::allocate_state

---
+++ src/basic/include/cppcon/basic/allocate_state.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <experimental/executor>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+namespace cppcon {
+namespace basic {
+
+namespace detail {
+
+template<typename CompletionHandler,
+         typename Head,
+         typename... Tail>
+decltype(auto) allocate_shared_find_completion_handler(const Head& head,
+                                                       const Tail&... tail) noexcept
+{
+  if constexpr (std::is_same_v<CompletionHandler,
+                               std::decay_t<Head>>)
+  {
+    return head;
+  } else {
+    return detail::allocate_shared_find_completion_handler<CompletionHandler>(tail...);
+  }
+}
+
+}
+
+template<typename State,
+         typename... Args>
+std::shared_ptr<State> allocate_state(Args&&... args) {
+  using completion_handler_type = typename State::completion_handler_type;
+  const auto& handler = detail::allocate_shared_find_completion_handler<completion_handler_type>(args...);
+  auto a = std::experimental::net::get_associated_allocator(handler,
+                                                            std::allocator<void>());
+  return std::allocate_shared<State>(a,
+                                     std::forward<Args>(args)...);
+}
+
+}
+}
