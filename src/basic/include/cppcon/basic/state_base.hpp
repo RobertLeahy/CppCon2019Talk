244d01f40be68d90cbee79c89563b7a7bfebb128
Robert Leahy <rleahy@rleahy.ca>
2019-07-27 15:38:46 +0000

    basic::state_base

---
+++ src/basic/include/cppcon/basic/state_base.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <cassert>
+#include <experimental/executor>
+#include <memory>
+#include <optional>
+#include <system_error>
+#include <type_traits>
+#include <utility>
+
+namespace cppcon {
+namespace basic {
+
+template<typename Executor,
+         typename CompletionHandler>
+class state_base {
+  Executor                       ex_;
+  CompletionHandler              handler_;
+  std::optional<std::error_code> ec_;
+public:
+  using completion_handler_type = CompletionHandler;
+  using executor_type = std::experimental::net::associated_executor_t<CompletionHandler,
+                                                                      Executor>;
+  using allocator_type = std::experimental::net::associated_allocator_t<CompletionHandler,
+                                                                        std::allocator<void>>;
+  state_base() = delete;
+  state_base(const state_base&) = delete;
+  state_base& operator=(const state_base&) = delete;
+  state_base(const Executor& ex,
+             CompletionHandler handler) noexcept(std::is_nothrow_move_constructible_v<CompletionHandler>)
+    : ex_     (ex),
+      handler_(std::move(handler))
+  {}
+  auto get_executor() const noexcept {
+    return std::experimental::net::get_associated_executor(handler_,
+                                                           ex_);
+  }
+  auto get_allocator() const noexcept {
+    return std::experimental::net::get_associated_allocator(handler_,
+                                                            std::allocator<void>());
+  }
+  template<typename Derived>
+  bool complete(const std::shared_ptr<Derived>& p,
+                std::error_code ec) noexcept
+  {
+    assert(p.get() == this);
+    if (!ec_) {
+      ec_ = ec;
+    }
+    return p.use_count() == 1;
+  }
+  template<typename Derived,
+           typename... Args>
+  void upcall(std::shared_ptr<Derived>& p,
+              Args... args) noexcept(noexcept(std::declval<CompletionHandler&>()(std::declval<std::error_code>(),
+                                                                                 std::declval<Args>()...)))
+  {
+    assert(ec_);
+    assert(p.use_count() == 1);
+    assert(p.get() == this);
+    auto handler = std::move(handler_);
+    auto ec = *ec_;
+    p.reset();
+    handler(ec,
+            args...);
+  }
+};
+
+}
+}
