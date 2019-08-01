8d11f5e6b3b3bac1e175912b1dd2749da84e6e24
Robert Leahy <rleahy@rleahy.ca>
2019-08-01 13:29:56 +0000

    basic::allocate_state: Simplification
    Using a std::tuple of const references and then std::get rather than
    rolling a helper function to pick out the completion handler.

--- src/basic/include/cppcon/basic/allocate_state.hpp
+++ src/basic/include/cppcon/basic/allocate_state.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <tuple>
 #include <type_traits>
 #include <utility>
 #include <boost/asio/ts/executor.hpp>
@@ -8,30 +9,11 @@
 namespace cppcon {
 namespace basic {
 
-namespace detail {
-
-template<typename CompletionHandler,
-         typename Head,
-         typename... Tail>
-decltype(auto) allocate_shared_find_completion_handler(const Head& head,
-                                                       const Tail&... tail) noexcept
-{
-  if constexpr (std::is_same_v<CompletionHandler,
-                               std::decay_t<Head>>)
-  {
-    return head;
-  } else {
-    return detail::allocate_shared_find_completion_handler<CompletionHandler>(tail...);
-  }
-}
-
-}
-
 template<typename State,
          typename... Args>
 std::shared_ptr<State> allocate_state(Args&&... args) {
-  using completion_handler_type = typename State::completion_handler_type;
-  const auto& handler = detail::allocate_shared_find_completion_handler<completion_handler_type>(args...);
+  std::tuple<const std::decay_t<Args>&...> t(args...);
+  auto&& handler = std::get<const typename State::completion_handler_type&>(t);
   auto a = boost::asio::get_associated_allocator(handler);
   return std::allocate_shared<State>(a,
                                      std::forward<Args>(args)...);
